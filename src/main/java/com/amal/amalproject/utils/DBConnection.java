3d85c85dda0564f2ad052674ea8d4a17f8983b11
kheireddine mechergui <kheireddine.mechergui@esprit.tn>
2022-08-20 21:54:37 +0100

    feat (database) Creating a singleton database connection class

---
+++ src/main/java/com/amal/amalproject/utils/DBConnection.java
@@ -0,0 +1,36 @@
+package com.amal.amalproject.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static Connection INSTANCE = null;
+
+    private DBConnection() {}
+
+    public static Connection getConnection() {
+
+        if (INSTANCE == null) {
+            try {
+                INSTANCE = DriverManager.getConnection("jdbc:mysql://localhost:3306/amal?useSSL=false","root","");
+            } catch (SQLException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+
+        return INSTANCE;
+    }
+
+}
+
+/**
+ *      JDBC (Specification)
+ *
+ *          MySQL : Implementation JDBC for MYSQL DATABASE
+ *          POSTGRES SQL : Implementation JDBC for POSTGRES SQL
+ *          ORACLE : Implementation JDBC for ORACLE
+ *          Microsoft Sql Server : Implementation JDBC for MS SQL Database
+ *
+ */
