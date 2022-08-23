53425fcb56bf78e3ea60d1dd6f2c140827a49e33
haithemyakoubii <haithem.yaakoubi@esprit.tn>
2022-08-23 03:58:06 +0100

    add entite

---
+++ src/main/java/com/amal/amalproject/entities/admin.java
@@ -0,0 +1,2 @@
+package com.amal.amalproject.entities;public class admin {
+}
---
+++ src/main/java/com/amal/amalproject/entities/beneficier.java
@@ -0,0 +1,14 @@
+package com.amal.amalproject;
+
+import java.util.Date;
+public class beneficier {
+    private int carte_handicap;
+    private Date date_expiration;
+    private int id_user;
+    private String nom;
+    private String prenom;
+    private String carte_img;
+
+
+
+}
