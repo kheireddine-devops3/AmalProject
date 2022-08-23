4f6dbe090c1d57219b07a5428d4fcf07ec939ee9
haithemyakoubii <haithem.yaakoubi@esprit.tn>
2022-08-23 12:02:18 +0100

    add controller

---
+++ src/main/java/com/amal/amalproject/models/DonsModel.java
@@ -0,0 +1,28 @@
+package com.amal.amalproject.models;
+
+import com.amal.amalproject.entities.dons;
+
+import java.util.List;
+
+public class DonsModel implements IDonsModel{
+
+    @Override
+    public dons addDons(dons DON) {
+        return null;
+    }
+
+    @Override
+    public dons removeDons(dons DON) {
+        return null;
+    }
+
+    @Override
+    public dons modifyDOns(dons DON) {
+        return null;
+    }
+
+    @Override
+    public List<dons> getAllDons() {
+        return null;
+    }
+}
