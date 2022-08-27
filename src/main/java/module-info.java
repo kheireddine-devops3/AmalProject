9b5419758b327e403d549ab0db1eaa9756e79f6a
Kheireddine Mechergui <kheireddine.mechergui@esprit.tn>
2022-08-27 20:51:47 +0100

    config (module-info) Update Config File

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,16 @@
 module com.amal.amalproject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
     requires java.sql;
+    requires org.controlsfx.controls;
+    requires org.apache.commons.codec;
+    requires org.apache.commons.io;
 
 
     opens com.amal.amalproject to javafx.fxml;
     exports com.amal.amalproject;
+    exports com.amal.amalproject.entities;
     exports com.amal.amalproject.controllers;
     opens com.amal.amalproject.controllers to javafx.fxml;
 }
