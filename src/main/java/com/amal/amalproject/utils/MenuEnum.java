d81bd16858bacb3fb81c76da0485c06ef5a14d6a
Kheireddine Mechergui <kheireddine.mechergui@esprit.tn>
2022-09-04 18:19:20 +0100

    feat (entities) Ajouter un enum pour bien contrôler les switches entre les vues

---
+++ src/main/java/com/amal/amalproject/utils/MenuEnum.java
@@ -0,0 +1,10 @@
+package com.amal.amalproject.utils;
+
+public enum MenuEnum {
+
+    MENU_RENDEZ_VOUS_VIEW,
+    MENU_CHOOSE_INSCRIPTION_VIEW,
+    MENU_INSCRIPTION_DOCTOR_VIEW,
+    MENU_LOGIN_VIEW,
+    MENU_EDIT_USER_VIEW;
+}
