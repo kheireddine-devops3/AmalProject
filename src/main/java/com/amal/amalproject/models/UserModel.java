a29818fea05cc3812278fb44eff55a7c16ef60eb
Amenikhlifi <ameni.khelifi@esprit.tn>
2022-09-04 21:10:50 +0100

    updating files

--- src/main/java/com/amal/amalproject/models/UserModel.java
+++ src/main/java/com/amal/amalproject/models/UserModel.java
@@ -35,14 +35,14 @@ public class UserModel implements IUserModel {
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
         try {
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM USER;");
+            PreparedStatement ps = connection.prepareStatement("SELECT * FROM user;");
             ResultSet resultSet = ps.executeQuery();
 
             while (resultSet.next()) {
 
-                int id = resultSet.getInt("ID");
-                String firstName = resultSet.getString("FIRSTNAME");
-                String lastName = resultSet.getString("LASTNAME");
+                int id = resultSet.getInt("id_user");
+                String firstName = resultSet.getString("nom_user");
+                String lastName = resultSet.getString("prenom_user");
 
                 users.add(new User(id, firstName, lastName));
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,15 @@ module com.amal.amalproject {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+	requires javafx.graphics;
 
 
     opens com.amal.amalproject to javafx.fxml;
+    opens com.amal.amalproject.entities to javafx.fxml;
+    opens com.amal.amalproject.models to javafx.fxml;
+    opens com.amal.amalproject.utils to javafx.fxml;
     exports com.amal.amalproject;
     exports com.amal.amalproject.controllers;
+    exports com.amal.amalproject.entities;
     opens com.amal.amalproject.controllers to javafx.fxml;
 }
